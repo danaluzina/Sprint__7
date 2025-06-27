d4fb6d930cf0c2e8c389e818e1f79c984e4a6e2b
Dana Luzina <danaluzina@yandex.ru>
2025-06-27 23:24:05 +0300

    develop commit

---
+++ src/test/java/order/GetListOfOrdersTest.java
@@ -0,0 +1,35 @@
+package order;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import courier.CourierClient;
+import model.Order;
+import order.OrderClient;
+import order.OrderGenerator;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GetListOfOrdersTest {
+
+    private static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
+    private int courierId;
+    CourierClient courierClient = new CourierClient();
+    OrderClient orderClient = new OrderClient();
+    private Order order;
+    private int orderTrack;
+    private int orderId;
+    @Before
+    public void setUp(){
+        RestAssured.baseURI = BASE_URL;
+
+        Order order = OrderGenerator.getOrderNoColors();
+
+    }
+    @Test
+    public void checkGetListOfOrders() {
+        Response response = orderClient.getOrdersList();
+        assertEquals("Код не соотвествует ожидаемому 200", 200, response.statusCode());
+        assertNotNull(response.body().path("orders"));
+    }
+}
